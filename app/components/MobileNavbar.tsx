4ef4e4aef151089b4b8f349cdd26d0ee0d6fcd5a
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2024-04-08 00:51:04 +0400

    finish navbar

---
+++ app/components/MobileNavbar.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+// import CartIcon from "./CartIcon";
+
+const links = [
+  { id: 1, title: "Homepage", url: "/" },
+  { id: 2, title: "Menu", url: "/menu" },
+];
+
+const MobileNavbar = () => {
+  const [open, setOpen] = useState(false);
+
+  // TEMPORARY
+  const user = false;
+  return (
+    <div>
+      <div className="flex justify-end">
+        {
+            open ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="fill-green-600 h-[25px]" onClick={() => setOpen(!open)}
+            ><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="fill-green-600 h-[25px]" onClick={() => setOpen(!open)}
+            ><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+        }
+      </div>
+      {open && (
+        <div className="bg-black text-green-600 absolute left-0  w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center z-10">
+          {links.map((item) => (
+            <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>
+              {item.title}
+            </Link>
+          ))}
+
+          {/* CHECK USER */}
+          {!user ? (
+            <Link href="/login" onClick={() => setOpen(false)}>
+              Login
+            </Link>
+          ) : (
+            <div className="flex flex-col gap-8">
+              <Link href="/orders" onClick={() => setOpen(false)}>
+                Orders
+              </Link>
+              <span>Logout</span>
+            </div>
+          )}
+          <Link href="/cart" onClick={() => setOpen(false)}>
+            {/* <CartIcon /> */}
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MobileNavbar;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+// import CartIcon from "./CartIcon";
+import MobileNavbar from "./MobileNavbar";
+
+const Navbar = () => {
+  const user = true;
+  return (
+    <div className="h-12 text-green-600 p-4 flex items-center justify-between border-b-2 bg-black uppercase md:h-[6rem] lg:px-20 xl:px-40">
+      {/* LOGO */}
+      <div className=" md:flex gap-4 flex-1 md:font-bold xl:text-xl">
+        <Link href="/">Rasta Pasta</Link>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <MobileNavbar />
+      </div>
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-12 items-center justify-end flex-1">
+        <Link href="/menu" className="group text-green-600 transition-all duration-300 ease-in-out">
+          <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out pb-2 xl:text-lg">Menu</span>
+        </Link>
+        {!user ? (
+          <Link href="/login" className="group text-green-600 transition-all duration-300 ease-in-out">
+            <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out pb-2 xl:text-lg">Login</span>
+        </Link>
+        ) : (
+          <div className="flex gap-12">
+            <Link href="/orders" className="group text-green-600 transition-all duration-300 ease-in-out">
+            <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-600 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out pb-2 xl:text-lg">Orders</span>
+            </Link>
+            <span className="cursor-pointer xl:text-lg">Logout</span>
+          </div>
+        )}
+        {/* <CartIcon /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
