e26c838e0101907a6b62a2e07d8f94843d7e2168
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2024-04-09 00:51:31 +0400

    finish menu page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { menu } from "@/app/data";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
 
 const MenuPage = () => {
   return (
-    <div>MenuPage</div>
-  )
-}
+    <div className="p-4 lg:px-20 lg:pb-12 xl:px-40 md:h-[calc(100vh-6rem)] flex flex-col md:flex-row items-center bg-black gap-8">
+      {menu.map((category) => (
+        <Link
+          href={`/menu/${category.slug}`}
+          key={category.id}
+          className="w-full opacity-80 hover:opacity-100 transition duration-200"
+        >
+          <div className=" bg-zinc-900 flex flex-col justify-center items-center gap-4 hover:bg-zinc-700 transition duration-200">
+            {category.img && <Image src={category.img} width={500} height={300} alt="" className="h-[10rem] lg:h-[18rem] object-cover" />}
+            <h1 className="uppercase font-bold text-3xl text-white">{category.title}</h1>
+            <p className="text-sm my-8 text-white px-4">{category.desc}</p>
+            <button className="hidden 2xl:block bg-green-600 hover:bg-green-400 transition duration-150 text-white py-2 px-4 rounded-md mb-4">Explore</button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
-export default MenuPage
+export default MenuPage;
