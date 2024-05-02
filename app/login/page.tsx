06c9101bafb14ac9939e90fc42ea924ac7f73ad0
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2024-05-02 15:12:46 +0400

    add register page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div>
+      LoginPage
+    <p>Don&apos;t have an account? <Link href="/register" className="text-blue-500 hover:text-blue-400">Create one!</Link></p>
+    </div>
   )
 }
 
