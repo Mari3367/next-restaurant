6b5a1b726af22051a373c2e6469462f55fca21a1
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2024-05-25 23:19:37 +0400

    add new product as admin link in navbar

--- app/components/CartIcon.tsx
+++ app/components/CartIcon.tsx
@@ -4,21 +4,37 @@ import React from "react";
 import { ShoppingCartIcon } from "@heroicons/react/20/solid";
 import { useCartStore } from "../utils/store";
 import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { PlusCircleIcon } from "@heroicons/react/20/solid";
 
 const CartIcon = () => {
   const { totalItems } = useCartStore();
+
+  const { data: session } = useSession();
 
   useEffect(() => {
     useCartStore.persist.rehydrate();
   }, []);
 
   return (
-    <Link href="/cart" className="flex items-center gap-4 md:pl-20">
-      <div className="relative w-8 h-8 md:w-5 md:h-5 hidden md:block">
-        <ShoppingCartIcon/>
-      </div>
-      <div className="xl:text-lg">Cart<span className="bg-yellow-400 ml-[2px] px-[4px] text-sm rounded-full">{totalItems}</span></div>
-    </Link>
+    <div>
+      {session && session.user.isAdmin ? (
+        <Link href="/add">
+          <div className="flex">
+            <span>Add New Product</span>
+            <PlusCircleIcon className="w-[20px]"/>
+          </div>
+        </Link>
+      ) : (
+        <Link href="/cart" className="flex items-center gap-4 md:pl-20">
+          <div className="relative w-8 h-8 md:w-5 md:h-5 hidden md:block">
+            <ShoppingCartIcon />
+          </div>
+          <div className="xl:text-lg">Cart<span className="bg-yellow-400 ml-[2px] px-[4px] text-sm rounded-full">{totalItems}</span></div>
+        </Link>
+      )}
+      
+    </div>
   );
 };
 
