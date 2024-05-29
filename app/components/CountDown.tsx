be4e82c5d8f64f36ac2e2c965f9b53c67bfcbd70
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2024-05-29 21:20:19 +0400

    fix hydration error in countdown component

--- app/components/CountDown.tsx
+++ app/components/CountDown.tsx
@@ -1,13 +1,43 @@
 "use client"
 import React from 'react'
-import Countdown from 'react-countdown'
+import { useState, useEffect } from "react";
 
-const endingDate = new Date("2024-06-25");
 
 const CountDown = () => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    const target = new Date("2024-06-29");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
+
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <Countdown className="font-bold text-2xl sm:text-5xl text-yellow-400" date={endingDate}/>
-  )
+    <span className="font-bold text-5xl text-yellow-300">
+      {days}:{hours}:{minutes}:{seconds}
+    </span>
+  );
+
 }
 
 export default CountDown;
